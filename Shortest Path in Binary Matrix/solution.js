147fe74eac4e92fb1c895487d3fe6c144479ac0b
Ethan Shealey <ethan.shealey@gmail.com>
2022-02-16 20:33:33 -0500

    Create solution.js

---
+++ Shortest Path in Binary Matrix/solution.js
@@ -0,0 +1,51 @@
+// create array of possible directions
+const DIRECTIONS = [
+    [0,1],
+    [0,-1],
+    [1,0],
+    [-1,0],
+    [-1,-1],
+    [-1,1],
+    [1,-1],
+    [1,1]
+]
+
+const shortestPathBinaryMatrix = (grid) => {
+    
+    // default case -- if no starting area
+    // end program
+    if(grid[0][0] === 1) return -1
+    
+    // initialize the queue and length
+    const queue = [[0,0,1]]
+    const n = grid.length
+    
+    // initialize the seen set
+    const seen = new Set('00')
+    
+    // while there still remains possible moves
+    while(queue.length) {
+        // get the row, col, and current step count from queue
+        let [r, c, s] = queue.pop()
+        // if reached end, return step count
+        if(r === n-1 && c === n-1) return s
+        // for each possible direction from node
+        for(const direction of DIRECTIONS) {
+            // get the new row and col
+            let nr = r + direction[0]
+            let nc = c + direction[1]
+            // check if direction is valid
+            // location is valid if:
+            //    * nr and nc do not exceed the play area
+            //    * seen set does not contain the new node
+            //    * the new node is not a 1
+            if(nr < 0 || nr >= n || nc < 0 || nc >=n || seen.has(nr + '' + nc) || grid[nr][nc] === 1) continue
+            // add new node to seen set
+            seen.add(nr + '' + nc)
+            // add new nodes to queue
+            queue.unshift([nr, nc, s + 1])
+        }
+    }
+    // no path found :(
+    return -1
+}
