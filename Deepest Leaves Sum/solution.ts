3c6c5e945062d6153b977357932f168adc305b53
Ethan Shealey <ethan.shealey@gmail.com>
2022-05-25 15:28:30 -0400

    Create solution.ts

---
+++ Deepest Leaves Sum/solution.ts
@@ -0,0 +1,18 @@
+let d: number = 0, sum: number = 0
+    
+    const solve = (node: TreeNode | null, depth: number): void => {
+        if(node.left === null && node.right === null) {
+            if(depth > d) {
+                d = depth
+                sum = node.val
+            }
+            else if(d === depth) sum += node.val
+            return
+        }
+        if(node.left) solve(node.left, depth+1)
+        if(node.right) solve(node.right, depth+1)
+    }
+    
+    solve(root, d)
+    return sum
+}
